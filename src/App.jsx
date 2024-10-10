c57fab6d3be07e363c56a4d4a16c2fe64ed707fe
Durgesh Katyayan <youngkong9601@gmail.com>
2024-10-10 15:28:57 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import { Route,Routes,BrowserRouter as Router } from 'react-router-dom'
+import './App.css'
+import TopSection from './components/TopSection.jsx'
+
+function App() {
+
+  return (
+    <Router>
+     <Routes>
+      <Route path='/' exact element={<TopSection/>}/>
+     </Routes>
+    </Router>
+  )
+}
+
+export default App
---
+++ src/components/TopSection.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const TopSection = () => {
+    return (
+        <>
+            <div className='top-section-body grid md:grid-cols-2 gap-2 px-5 '>
+                <div className='border-2'>ddgf</div>
+                <div className='md:px-5 px-3 py-5 mt-10 border-2 text-right'>
+                    <h1 className='text-3xl md:text-5xl text-orange-500 font-bold md:font-extrabold '>History of</h1>
+                    <h2 className=''>ALABAY</h2>
+                    <pre>The Central Asian Shepherd Dog,</pre>
+                    <pre>also known as Alabay, has been a</pre>
+                    <pre>guardian of livestock and property</pre>
+                    <pre>for centuries. Originating from</pre>
+                    <pre>Central Asia, these dogs are</pre>
+                    <pre>renowned for their courage,</pre>
+                    <pre>strength, and loyalty.</pre>
+
+
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default TopSection
